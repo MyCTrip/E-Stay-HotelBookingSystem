fd3102d4136cca5de07dc85c106108c2a7f64b60
HuangChen <a17773238940@gmail.com>
2026-01-31 21:13:50 +0800

    refactor: 重构前端项目结构 (React18 + TS + Vite)

---
+++ pc-admin/src/pages/merchant/HotelEntry/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const HotelEntry: React.FC = () => {
+  return (
+    <div style={{ padding: 24 }}>
+      <Typography.Title level={2}>酒店信息录入</Typography.Title>
+      <p>这里将放置复杂的表单组件...</p>
+    </div>
+  );
+};
+
+export default HotelEntry;
